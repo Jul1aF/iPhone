2c81f64abacae9815a1fd90bb7b72140cc8298a9
Juli4f <lia.pkjm@gmail.com>
2023-10-22 02:01:52 -0300

    Impantação da UML

---
+++ src/iPhone.java
@@ -0,0 +1,48 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+
+    @Override
+    public void ligar(String numero) {
+        //Codigo
+    }
+
+    @Override
+    public void atender(String numero) {
+        //Codigo
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        //Codigo
+    }
+
+    @Override
+    public void exibirPagina(String link) {
+        //Codigo
+    }
+
+    @Override
+    public void adicionarNovaAba(String link) {
+        //Codigo
+    }
+
+    @Override
+    public void atualizarPagina() {
+        //Codigo
+    }
+
+    @Override
+    public void tocar() {
+        //Codigo
+    }
+
+    @Override
+    public void pausar() {
+        //Codigo
+    }
+
+    @Override
+    public void selecionarMusica() {
+        //Codigo
+    }
+}
